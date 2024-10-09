12df17e850c6fceddd60730d86c5295adb25831a
Kishor Satishkumar Kashid <kashid.k@northeastern.edu>
2024-10-09 15:51:24 -0400

    Worked on CI

--- app/tests/userIntegrationTest.test.js
+++ app/tests/userIntegrationTest.test.js
@@ -17,7 +17,7 @@ describe('User Routes Integration Tests', () => {
   });
 
   describe('POST /user', () => {
-    it('should not create a user and return 400', async () => {
+    it('should create a user and return 201', async () => {
       const userData = {
         first_name: 'kishor',
         last_name: 'kashid',
@@ -28,9 +28,7 @@ describe('User Routes Integration Tests', () => {
       const response = await request(app)
         .post('/v1/user')
         .send(userData)
-        .expect(400);
-
-      expect(response.body.message).toContain('User already exists with this email');
+        .expect(201);
     });
 
     it('should return 400 if invalid data is provided', async () => {
@@ -55,36 +53,31 @@ describe('User Routes Integration Tests', () => {
     let user;
 
     beforeEach(async () => {
-      // Create a user for testing
-      user = ({
+      // Insert the user into the database for testing
+      user = await User.create({
         first_name: 'kishor',
         last_name: 'kashid',
         email: 'kishorkashid1@gmail.com',
-        password: await bcrypt.hash('Kishor@1', 10),
+        password: await bcrypt.hash('Kishor@1', 10), // Store the hashed password
       });
     });
 
     it('should return user data for the authenticated user', async () => {
+      // Encode the email and password for Basic Auth
       const base64Token = Buffer.from(`${user.email}:Kishor@1`).toString('base64');
-      
+
+      // Make the request with Basic Auth
       const response = await request(app)
         .get('/v1/user/self')
         .set('Authorization', `Basic ${base64Token}`)
         .expect(200);
 
+      // Validate the response contains the correct user data
       expect(response.body.email).toBe(user.email);
       expect(response.body.first_name).toBe(user.first_name);
       expect(response.body.last_name).toBe(user.last_name);
     });
-
-    it('should return 400 if query parameters are present', async () => {
-      const base64Token = Buffer.from(`${user.email}:Kishor@1`).toString('base64');
-
-      const response = await request(app)
-        .get('/v1/user/self?name="kishor"')
-        .set('Authorization', `Basic ${base64Token}`)
-        .expect(400);
-    });
   });
+
 });
 

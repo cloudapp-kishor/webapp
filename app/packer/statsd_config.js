35fb40b1f4e4cae52817e8fbd0f4195516c768f8
Kishor Satishkumar Kashid <kashid.k@northeastern.edu>
2024-10-30 09:44:19 -0400

    Worked on metric

--- app/packer/statsd_config.js
+++ app/packer/statsd_config.js
@@ -1,10 +1,8 @@
-require('dotenv').config({ path: '../../.env' });
-
 module.exports = {
     port: 8125,
     backends: ["statsd-cloudwatch-backend"],
     cloudwatch: {
         namespace: "MyApp",
-        region: "process.env.REGION"
+        region: process.env.AWS_REGION
     }
 };
